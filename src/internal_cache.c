fc8a501791e080507f7f2eeb9454eebe1e271df4
Mathew Heard <splitice@users.noreply.github.com>
2017-10-31 09:00:45 +1100

    send latest on alloc failure, not cached

--- src/internal_cache.c
+++ src/internal_cache.c
@@ -176,7 +176,7 @@ internal_cache_ct_resync(enum nf_conntrack_msg_type type,
 			sync_send(obj2, NET_T_STATE_CT_UPD);
 			cache_ct_free(obj2);
 		}else{
-			sync_send(obj->ptr, NET_T_STATE_CT_UPD);
+			sync_send(ct, NET_T_STATE_CT_UPD);
 		}
 		break;
 	}
